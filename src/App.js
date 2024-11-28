d6aa514b73a5b1c287aca5d3169d49b9d7c343d7
Chamodya B Herath <134180331+CA-Creation@users.noreply.github.com>
2024-11-28 09:57:29 +0530

    75 finished

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Typography } from "@material-ui/core";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import { common } from "@material-ui/core/colors";
+import wordsToNumbers from "words-to-numbers";
 
 import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from "./styles";
 
-const alanKey = 'b9d38d7d14fdb96a4fd1c1d57bc754bb2e956eca572e1d8b807a3e2338fdd0dc/stage';
+import alan_logo from "../src/images/alan_logo.png";
+
+const alankey ="b9d38d7d14fdb96a4fd1c1d57bc754bb2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
-    const [newsArticles, setNewsArticles] = useState([]);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-        alanBtn({
-            key: alanKey,
-            onCommand: ({ command, articles }) => {
-                if(command === 'newHeadlines'){
-                   setNewsArticles(articles);
-                }
-            }
-        })
-    }, [])
+  const classes = useStyles();
 
-    return (
-        <div>
-            <h1>Alan Ai news application</h1>
-            <NewsCards articles={newsArticles} />
-        </div>
-    );
-}
+
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img className={classes.alanLogo} src={alan_logo} alt="alan logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  );
+};
 
 export default App;
